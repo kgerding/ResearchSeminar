f79a05f97c185e96bf78e4f170e537b6f15abdfb
Kilian Gerding <kiliangerdign@web.de>
2021-03-12 15:54:17 +0100

    subsetting

--- Data_cleaning.py
+++ Data_cleaning.py
@@ -22,11 +22,9 @@ print(relative_path)
 # Step 1: Read in Data
 ahs_2019 = pd.read_csv("Data/AHS 2019 National PUF v1.1 CSV/household_national.csv", index_col=0)
 
-ahs_2019
-
 # Step 2: get the right values
-drop_values = pd.read_csv("Data/codebook_2019.csv")
+drop_values = pd.read_excel("/Users/kiliangerding/OneDrive - Universität St.Gallen/MBF_2_Semester/Courses/Research Seminar/Data/Info_on_Data/codebook_2019.xlsx")
 drop_values = drop_values[drop_values.Include == 'x']
-drop_values = drop_values[['Variable']]
+drop_values = pd.DataFrame(columns=drop_values['Variable'].values)
 
-
+ahs_2019 = ahs_2019[(drop_values.columns) & (ahs_2019.columns)]
