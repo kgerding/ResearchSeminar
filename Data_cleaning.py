a0c194262383977bac46c0adcfe949a6f5c0e941
Kilian Gerding <kiliangerdign@web.de>
2021-03-12 12:53:53 +0100

    Update Data_cleaning.py

--- Data_cleaning.py
+++ Data_cleaning.py
@@ -3,6 +3,11 @@
 import pandas as pd
 
 # Step 1: Read in Data
-path = "/Users/kiliangerding/OneDrive - Universität St.Gallen/MBF_2_Semester/Courses/Research Seminar/Data/AHS 2015 National PUF v3.1 CSV/household.csv"
+# path Kilian
+# path = "/Users/kiliangerding/OneDrive - Universität St.Gallen/MBF_2_Semester/Courses/Research Seminar/Data/AHS 2015 National PUF v3.1 CSV/household.csv"
+
+# path Tim
+# path
+
 ahs_2019 =  pd.read_csv(path, index_col=0)
 ahs_2019
