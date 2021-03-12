e76666d0244dd0dac0b56d9d5d7f4f4c8e8df317
timggraf <tim.g.graf@gmail.com>
2021-03-12 13:21:27 +0100

    gitignore

--- Data_cleaning.py
+++ Data_cleaning.py
@@ -1,8 +1,28 @@
 #This file is for data cleaning
-
+import os
 import pandas as pd
+import numpy as np
 
 # Step 1: Read in Data
 path = "/Users/kiliangerding/OneDrive - Universität St.Gallen/MBF_2_Semester/Courses/Research Seminar/Data/AHS 2015 National PUF v3.1 CSV/household.csv"
 ahs_2019 =  pd.read_csv(path, index_col=0)
 ahs_2019
+
+# Step 2: get the right values
+path2 = "/"
+
+# Path
+path = "/home / User / Desktop / file.txt"
+
+# Path of Start directory
+start = "/home / User"
+
+# Compute the relative file path
+# to the given path from the
+# the given start directory.
+relative_path = os.path.relpath(path, start)
+
+# Print the relative file path
+# to the given path from the
+# the given start directory.
+print(relative_path)
