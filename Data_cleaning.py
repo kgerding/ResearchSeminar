15e1acdb513107e5ee507d9914c2464c98ed88f1
timggraf <tim.g.graf@gmail.com>
2021-03-12 13:45:19 +0100

    Update Data_cleaning.py

--- Data_cleaning.py
+++ Data_cleaning.py
@@ -3,24 +3,11 @@ import os
 import pandas as pd
 import numpy as np
 
-# Step 1: Read in Data
-# path Kilian
-# path = "/Users/kiliangerding/OneDrive - Universität St.Gallen/MBF_2_Semester/Courses/Research Seminar/Data/AHS 2015 National PUF v3.1 CSV/household.csv"
-
-# path Tim
-# path
-
-ahs_2019 =  pd.read_csv(path, index_col=0)
-ahs_2019
-
-# Step 2: get the right values
-path2 = "/"
-
 # Path
-path = "/home / User / Desktop / file.txt"
+path = "/Users/tgraf/Google Drive/Uni SG/Master/Research Seminar /Repository/Data"
 
 # Path of Start directory
-start = "/home / User"
+start = "/Users/tgraf/Google Drive/Uni SG/Master/Research Seminar /Repository/"
 
 # Compute the relative file path
 # to the given path from the
@@ -31,3 +18,15 @@ relative_path = os.path.relpath(path, start)
 # to the given path from the
 # the given start directory.
 print(relative_path)
+
+# Step 1: Read in Data
+ahs_2019 = pd.read_csv("Data/AHS 2019 National PUF v1.1 CSV/household_national.csv", index_col=0)
+
+ahs_2019
+
+# Step 2: get the right values
+drop_values = pd.read_csv("Data/codebook_2019.csv")
+drop_values = drop_values[drop_values.Include == 'x']
+drop_values = drop_values[['Variable']]
+
+
