c34870b6b0dc60cab1b7d38e29dfa2325e3314e8
timggraf <tim.g.graf@gmail.com>
2021-03-19 10:40:15 +0100

    Update Data_cleaning.py

--- Data_cleaning.py
+++ Data_cleaning.py
@@ -4,10 +4,8 @@ import pandas as pd
 import numpy as np
 
 # Path
-path = "/Users/tgraf/Google Drive/Uni SG/Master/Research Seminar /Repository/Data"
-
-# Path of Start directory
-start = "/Users/tgraf/Google Drive/Uni SG/Master/Research Seminar /Repository/"
+path = os.path.abspath(os.getcwd())
+print(path)
 
 # Compute the relative file path
 # to the given path from the
@@ -20,12 +18,14 @@ relative_path = os.path.relpath(path, start)
 print(relative_path)
 
 # Step 1: Read in Data
-ahs_2019 = pd.read_csv("Data/AHS 2019 National PUF v1.1 CSV/household_national.csv", index_col=0)
+ahs_2019 = pd.read_csv(path + "/Data/AHS 2019 National PUF v1.1 CSV/household_national.csv", index_col=0)
 
 # Step 2: get the right values
-drop_values = pd.read_excel("/Users/kiliangerding/OneDrive - Universität St.Gallen/MBF_2_Semester/Courses/Research Seminar/Data/Info_on_Data/codebook_2019.xlsx")
+drop_values = pd.read_excel(Data/Info_on_Data/codebook_2019.xlsx")
 drop_values = drop_values[drop_values.Include == 'x']
 drop_values = pd.DataFrame(columns=drop_values['Variable'].values)
 
 ahs_2019 = ahs_2019[(drop_values.columns) & (ahs_2019.columns)]
 ahs_2019.MARKETVAL[ahs_2019.MARKETVAL != -6]
+
+os.path.relpath()
