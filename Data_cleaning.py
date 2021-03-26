d496abf27abb992dd6dea18741d544a75995499a
Kilian Gerding <kiliangerdign@web.de>
2021-03-26 09:35:19 +0100

    c

--- Data_cleaning.py
+++ Data_cleaning.py
@@ -29,9 +29,45 @@ ahs_2019_marketval = ahs_2019[(ahs_2019.MARKETVAL != -6)]
 counted_ahs_both = ahs_2019_both[ahs_2019_both != -6].count()/ahs_2019_both.shape[0]
 counted_ahs_marketval = ahs_2019_marketval[ahs_2019_marketval != -6].count()/ahs_2019_marketval.shape[0]
 
-ahs_2019_both.head()
 
 # Step 3: Match CSMB
 longlat_2018 = pd.read_csv(path + "/Data/cbsa-boundaries-2018.csv", sep=';')
-longlat_2018.loc[:,"Year"].unique()
-longlat_2018.columns
+longlat_2018 = longlat_2018.loc[:,["CBSAFP","Geo Point"]]
+longlat_2018[['long', 'lat']] = longlat_2018['Geo Point'].str.split(',', expand=True)
+longlat_2018 = longlat_2018.drop('Geo Point', 1)
+longlat_2018. columns = ['OMB13CBSA', 'longitude', 'latitude']
+
+
+# Step 4: Merge with ahs data
+newdf = ahs_2019.merge(longlat_2018, on=['OMB13CBSA'])
+
+import matplotlib 
+import mpl_toolkits
+from mpl_toolkits.basemap import Basemap
+
+# 1. Draw the map background
+fig = plt.figure(figsize=(8, 8))
+m = Basemap(projection='lcc', resolution='h', 
+            lat_0=37.5, lon_0=-119,
+            width=1E6, height=1.2E6)
+m.shadedrelief()
+m.drawcoastlines(color='gray')
+m.drawcountries(color='gray')
+m.drawstates(color='gray')
+
+# 2. scatter city data, with color reflecting population
+# and size reflecting area
+m.scatter(lon, lat, latlon=True,
+          c=np.log10(population), s=area,
+          cmap='Reds', alpha=0.5)
+
+# 3. create colorbar and legend
+plt.colorbar(label=r'$\log_{10}({\rm population})$')
+plt.clim(3, 7)
+
+# make legend with dummy points
+for a in [100, 300, 500]:
+    plt.scatter([], [], c='k', alpha=0.5, s=a,
+                label=str(a) + ' km$^2$')
+plt.legend(scatterpoints=1, frameon=False,
+           labelspacing=1, loc='lower left');
