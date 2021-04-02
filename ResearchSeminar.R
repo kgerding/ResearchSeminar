905dabc4812d0851a6960a4f00789ae9c04cbb6b
Kilian Gerding <kiliangerdign@web.de>
2021-04-02 11:12:20 +0200

    them changes

--- ResearchSeminar.R
+++ ResearchSeminar.R
@@ -96,13 +96,35 @@ abline(v=35, col="red")
 #delete columns
 p2016 <- p2016[, which(colSums(is.na(p2016)/nrow(p2016)) < 0.2)]
 
+# select hedonics
+hedonics <- c('id_parcel','num_bathroom','num_bedroom','area_live_finished',
+              'flag_tub_or_spa','loc_latitude','loc_longitude','area_lot','type_zoning_landuse_county',
+              'type_zoning_landuse','loc_zip','loc_county', 'year_built', 'flag_fireplace', 'num_tax_building',
+              'num_tax_total', 'num_tax_land')
+p2016 <- p2016[, hedonics ]
+
+# transform dummies and factors
+p2016$flag_tub_or_spa[p2016$flag_tub_or_spa == 'true'] <- 1
+p2016$flag_tub_or_spa[p2016$flag_tub_or_spa != 'true'] <- 0
+p2016$flag_fireplace[p2016$flag_fireplace == 'true'] <- 1
+p2016$flag_fireplace[p2016$flag_fireplace != 'true'] <- 0
+p2016$flag_tub_or_spa <- as.numeric(p2016$flag_tub_or_spa)
+p2016$flag_fireplace <- as.numeric(p2016$flag_fireplace)
+
 # clean house prices
 hist(p2016$num_tax_total[p2016$num_tax_total < 1000000], breaks = 100)
 p2016 <- p2016[p2016$num_tax_total >= 50000,]
 
+summary(p2016) 
 
-summarise(col(p2016))
+
 # select only relevant
-p2016 <- p2016[, -c('type_zoning_landuse_county', 'type_zoning_landuse', 'type_desc_zoning_landuse', 'loc_raw_tract_bloc')]
 test <- na.omit(p2016)
-within(test, rm(x, y))
+
+
+# lm 
+hedonic <- lm(num_tax_building ~ num_bathroom + num_bedroom + area_live_finished + area_lot + year_built, data = p2016)
+summary(hedonic)
+
+
+
