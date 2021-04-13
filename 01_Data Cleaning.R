a7ddb79c43a7cb05620612fed1afea91a0e1c7e2
Kilian Gerding <kiliangerdign@web.de>
2021-04-13 12:24:05 +0200

    changes

--- 01_Data Cleaning.R
+++ 01_Data Cleaning.R
@@ -27,245 +27,256 @@ rm(list=ls())
 # prices from Zillow transactions 2016 and 2017
 prices2016 <- read.csv('./Data/properties_2016.csv', sep = ',')
 prices2017 <- read.csv('./Data/properties_2017.csv', sep = ',')
+
+# additional factor description
 id <- read.csv('./Info on Data/id.csv', sep = ',')
 heating_id <- read.csv('./Info on Data/heating_id.csv', sep = ',')
 quality_id <- read.csv('./Info on Data/quality_id.csv', sep = ',')
 
-### PART 1: DATA INSPECTION ----------------------------------------------------
-
-## Step 0: Rename the Data
-
-p2016 <- prices2016 %>% rename(
-  id_parcel = parcelid,
-  year_built = yearbuilt,
-  area_basement = basementsqft,
-  area_patio = yardbuildingsqft17,
-  area_shed = yardbuildingsqft26, 
-  area_pool = poolsizesum,  
-  area_lot = lotsizesquarefeet, 
-  area_garage = garagetotalsqft,
-  area_firstfloor_finished = finishedfloor1squarefeet,
-  area_total_calc = calculatedfinishedsquarefeet,
-  area_base = finishedsquarefeet6,
-  area_live_finished = finishedsquarefeet12,
-  area_liveperi_finished = finishedsquarefeet13,
-  area_total_finished = finishedsquarefeet15,  
-  area_unknown = finishedsquarefeet50,
-  num_unit = unitcnt, 
-  num_story = numberofstories,  
-  num_room = roomcnt,
-  num_bathroom = bathroomcnt,
-  num_bedroom = bedroomcnt,
-  num_bathroom_calc = calculatedbathnbr,
-  num_bath = fullbathcnt,  
-  num_75_bath = threequarterbathnbr, 
-  num_fireplace = fireplacecnt,
-  num_pool = poolcnt,  
-  num_garage = garagecarcnt,  
-  num_tax_total = taxvaluedollarcnt,
-  num_tax_building = structuretaxvaluedollarcnt,
-  num_tax_land = landtaxvaluedollarcnt,
-  num_tax_property = taxamount,
-  tax_assess_year = assessmentyear,
-  tax_delinquency_flag = taxdelinquencyflag,
-  tax_delinquency_year = taxdelinquencyyear,
-  loc_county = regionidcounty,
-  loc_city = regionidcity,
-  loc_zip = regionidzip,
-  loc_neighbor = regionidneighborhood, 
-  loc_fips = fips,
-  loc_tract_block = censustractandblock,
-  loc_raw_tract_block = rawcensustractandblock,
-  loc_longitude = longitude,
-  loc_latitude = latitude,
-  flag_fireplace = fireplaceflag, 
-  flag_tub_or_spa = hashottuborspa,
-  flag_spa_or_tub_pool = pooltypeid2,
-  flag_no_tub = pooltypeid7,
-  flag_spa_or_tub = pooltypeid10,
-  type_desc_zoning_landuse = propertyzoningdesc,
-  type_zoning_landuse = propertylandusetypeid,
-  type_zoning_landuse_county = propertycountylandusecode,
-  type_quality = buildingqualitytypeid,
-  type_framing = buildingclasstypeid,
-  type_material = typeconstructiontypeid,
-  type_deck = decktypeid,
-  type_story = storytypeid,
-  type_heating = heatingorsystemtypeid,
-  type_ac = airconditioningtypeid,
-  type_architect = architecturalstyletypeid
-)
-
+# change colnames
 colnames(id) <- c('type_zoning_landuse', 'factor')
 colnames(heating_id) <- c('type_heating', 'heating_factor')
 colnames(quality_id) <- c('type_quality', 'quality_factor')
 
-## Step 1: Transform data ------------------------------------------------------
+### PART 1: DATA INSPECTION ----------------------------------------------------
 
-# transform dummies and factors
-p2016$flag_tub_or_spa[p2016$flag_tub_or_spa == 'true'] <- 1
-p2016$flag_tub_or_spa[p2016$flag_tub_or_spa != '1'] <- 0
-p2016$flag_fireplace[p2016$flag_fireplace == 'true'] <- 1
-p2016$flag_fireplace[p2016$flag_fireplace != '1'] <- 0
-p2016$flag_tub_or_spa <- as.numeric(as.character(p2016$flag_tub_or_spa))
-p2016$flag_fireplace <- as.numeric(as.character(p2016$flag_fireplace))
+all_data <- list(prices2016, prices2017)
 
-# add age insted of year_built
-p2016$age <- 2021 - p2016$year_built
+for (i in c(1,2)) {
 
-# type id as factor
-p2016 <- left_join(p2016, id, by = 'type_zoning_landuse')
-p2016 <- p2016[ , -which(names(p2016) %in% c("type_zoning_landuse"))]
-p2016$factor <- as.factor(p2016$factor)
+  ## Step 0: Rename the Data
+  
+  i <- all_data[[1]]
+  
+  p2016 <- i %>% rename(
+    id_parcel = parcelid,
+    year_built = yearbuilt,
+    area_basement = basementsqft,
+    area_patio = yardbuildingsqft17,
+    area_shed = yardbuildingsqft26, 
+    area_pool = poolsizesum,  
+    area_lot = lotsizesquarefeet, 
+    area_garage = garagetotalsqft,
+    area_firstfloor_finished = finishedfloor1squarefeet,
+    area_total_calc = calculatedfinishedsquarefeet,
+    area_base = finishedsquarefeet6,
+    area_live_finished = finishedsquarefeet12,
+    area_liveperi_finished = finishedsquarefeet13,
+    area_total_finished = finishedsquarefeet15,  
+    area_unknown = finishedsquarefeet50,
+    num_unit = unitcnt, 
+    num_story = numberofstories,  
+    num_room = roomcnt,
+    num_bathroom = bathroomcnt,
+    num_bedroom = bedroomcnt,
+    num_bathroom_calc = calculatedbathnbr,
+    num_bath = fullbathcnt,  
+    num_75_bath = threequarterbathnbr, 
+    num_fireplace = fireplacecnt,
+    num_pool = poolcnt,  
+    num_garage = garagecarcnt,  
+    num_tax_total = taxvaluedollarcnt,
+    num_tax_building = structuretaxvaluedollarcnt,
+    num_tax_land = landtaxvaluedollarcnt,
+    num_tax_property = taxamount,
+    tax_assess_year = assessmentyear,
+    tax_delinquency_flag = taxdelinquencyflag,
+    tax_delinquency_year = taxdelinquencyyear,
+    loc_county = regionidcounty,
+    loc_city = regionidcity,
+    loc_zip = regionidzip,
+    loc_neighbor = regionidneighborhood, 
+    loc_fips = fips,
+    loc_tract_block = censustractandblock,
+    loc_raw_tract_block = rawcensustractandblock,
+    loc_longitude = longitude,
+    loc_latitude = latitude,
+    flag_fireplace = fireplaceflag, 
+    flag_tub_or_spa = hashottuborspa,
+    flag_spa_or_tub_pool = pooltypeid2,
+    flag_no_tub = pooltypeid7,
+    flag_spa_or_tub = pooltypeid10,
+    type_desc_zoning_landuse = propertyzoningdesc,
+    type_zoning_landuse = propertylandusetypeid,
+    type_zoning_landuse_county = propertycountylandusecode,
+    type_quality = buildingqualitytypeid,
+    type_framing = buildingclasstypeid,
+    type_material = typeconstructiontypeid,
+    type_deck = decktypeid,
+    type_story = storytypeid,
+    type_heating = heatingorsystemtypeid,
+    type_ac = airconditioningtypeid,
+    type_architect = architecturalstyletypeid
+  )
+  
+  ## Step 1: Transform data ------------------------------------------------------
+  
+  # transform dummies and factors
+  p2016$flag_tub_or_spa[p2016$flag_tub_or_spa == 'true'] <- 1
+  p2016$flag_tub_or_spa[p2016$flag_tub_or_spa != '1'] <- 0
+  p2016$flag_fireplace[p2016$flag_fireplace == 'true'] <- 1
+  p2016$flag_fireplace[p2016$flag_fireplace != '1'] <- 0
+  p2016$flag_tub_or_spa <- as.numeric(as.character(p2016$flag_tub_or_spa))
+  p2016$flag_fireplace <- as.numeric(as.character(p2016$flag_fireplace))
+  
+  # add age insted of year_built
+  p2016$age <- 2021 - p2016$year_built
+  
+  # type id as factor
+  p2016 <- left_join(p2016, id, by = 'type_zoning_landuse')
+  p2016 <- p2016[ , -which(names(p2016) %in% c("type_zoning_landuse"))]
+  p2016$factor <- as.factor(p2016$factor)
+  
+  # heating id as factor
+  p2016 <- left_join(p2016, heating_id, by = 'type_heating')
+  p2016 <- p2016[ , -which(names(p2016) %in% c("type_heating"))]
+  p2016$heating_factor <- as.factor(p2016$heating_factor)
+  
+  # type id as factor
+  p2016 <- left_join(p2016, quality_id, by = 'type_quality')
+  p2016 <- p2016[ , -which(names(p2016) %in% c("type_quality"))]
+  p2016$quality_factor <- as.factor(p2016$quality_factor)
+  
+  # proportion of living area to area lot
+  p2016$prop_living <- p2016$area_live_finished/p2016$area_lot
+  
+  # proportion of building to land value
+  p2016$build_land_prop <- p2016$num_tax_building/p2016$num_tax_land
+  
+  # filter no baths and no bedrooms, we aim to separate properties with buildings
+  # and properties without buildings
+  property_only16 <- p2016[(p2016$num_bathroom == 0 & p2016$num_bedroom == 0),]
+  house_only16 <- p2016[(p2016$num_bathroom > 0 | p2016$num_bedroom > 0),]
+  
+  ## Step 4: Eliminate columns with more than 20% NAs -------------------
+  
+  # first remove NAs of most important columns to see an more workable data set
+  # delete 7126 rows
+  house_only16 <- house_only16[(na.omit(house_only16$num_bathroom) &
+                                  na.omit(house_only16$num_bedroom) &
+                                  na.omit(house_only16$num_tax_total) ), ]
+  
+  # quick plot
+  count_nas <- colSums(is.na(house_only16))/nrow(house_only16)
+  sorted <- rev(sort(count_nas))
+  barplot(sorted, cex.names = 0.5, las = 2)
+  abline(v=35, col="red")
+  
+  
+  # define and select hedonics (<0.2 NAs or <0.4 NAs)
+  hedonics <- c('id_parcel','num_bathroom','num_bedroom','area_live_finished',
+                'flag_tub_or_spa','loc_latitude','loc_longitude','area_lot',
+                'factor','loc_zip','loc_county', 'age',
+                'flag_fireplace', 'num_tax_building','num_tax_total',
+                'num_tax_land', 'prop_living', 'build_land_prop')
+  
+  hedonics2 <- c('id_parcel','num_bathroom','num_bedroom','area_live_finished',
+                'flag_tub_or_spa','loc_latitude','loc_longitude','area_lot',
+                'factor','loc_zip','loc_county', 'age',
+                'flag_fireplace', 'num_tax_building','num_tax_total',
+                'num_tax_land', 'num_unit', 'quality_factor', 'heating_factor',
+                'prop_living', 'build_land_prop')
+  
+  house_only16_mv <- house_only16 %>% select(hedonics2)
+  house_only16 <- house_only16 %>% select(hedonics)
+  
+  # finally remove all NAs as not usefull for regression and ML
+  #house_only16 <- na.omit(house_only16)
+  #house_only16_mv <- na.omit(house_only16_mv)
+  
+  
+  ## Step 5: Eliminate properties without buildings and very low values ----------
+  
+  # drop building values below 50'000
+  hist(house_only16$num_tax_building[house_only16$num_tax_building < 500000],
+       breaks = 100)
+  
+  house_only16 <- house_only16[house_only16$num_tax_total >= 50000,]
+  
+  ## Step 5: Plot the variable relationships and remove outliers -------------------------------
+  # plot bedroom vs tax
+  ggplot(data = house_only16[1:100000,], aes(x = num_bedroom, y = log(num_tax_building))) +
+    geom_point()
+  
+  # plot bedroom vs tax
+  ggplot(data = house_only16[1:100000,], aes(x = num_bathroom, y = log(num_tax_building))) +
+    geom_point()
+  
+  # plot size vs tax
+  ggplot(data = house_only16[1:100000,], aes(x = area_live_finished, y = (num_tax_building))) +
+    geom_point()
+  
+  # we need to filter the outlier of high area_live finished: 
+  house_only16 <- house_only16[house_only16$area_live_finished < 58000,]
+  
+  ggplot(data = house_only16[1:100000,], aes(x = area_live_finished, y = (num_tax_building))) +
+    geom_point() 
+  
+  # plot age vs tax
+  ggplot(data = house_only16[1:100000,], aes(x = age, y = (log(num_tax_building)))) +
+    geom_point() 
+  
+  # plot area_lot vs tax
+  ggplot(data = house_only16[1:100000,], aes(x = area_lot, y = (num_tax_building))) +
+    geom_point() 
+  
+  # we need to filter the outlier of high area_lot but very low building structure value 
+  house_only16 <- house_only16[house_only16$area_lot < 1e7,]
+  
+  
+  ### PART 2 ALGORITHMS ###---------------------------------------------------
+  
+  ## Regressions ------------------------------------
+  
+  # simple regression of building value
+  hedonic_build <- lm(log(num_tax_building) ~ num_bathroom + num_bedroom + area_live_finished + 
+                  flag_tub_or_spa + area_lot + age + flag_fireplace #+ prop_living + build_land_prop
+                  ,data = house_only16)
+  
+  summary(hedonic_build)
+  
+  # simple regression of total value
+  hedonic_total <- lm(log(num_tax_total) ~ num_bathroom + num_bedroom + area_live_finished + 
+                  flag_tub_or_spa + area_lot + age + flag_fireplace #+ prop_living + build_land_prop
+                  ,
+                  data = house_only16)
+  
+  summary(hedonic_total)
+  
+  
+  # lm of building value with factors
+  hedonic_total_fact <- lm(log(num_tax_building) ~ num_bathroom + num_bedroom + area_live_finished + 
+                  flag_tub_or_spa + area_lot + age + flag_fireplace #+ prop_living + build_land_prop
+                  + factor
+                  , 
+                  data = house_only16)
+  
+  summary(hedonic_total_fact)
+  
+  hedonic_total_fact2 <- lm(log(num_tax_building) ~ num_bathroom + num_bedroom + area_live_finished + 
+                             flag_tub_or_spa + area_lot + age + flag_fireplace #+ prop_living + build_land_prop
+                           + factor + num_unit + quality_factor + heating_factor
+                           , 
+                           data = house_only16_mv)
+  
+  summary(hedonic_total_fact2)
+  
+  # lm of building value with factors
+  hedonic_total_fact <- lm(log(num_tax_total) ~ num_bathroom + num_bedroom + area_live_finished + 
+                             flag_tub_or_spa + area_lot + age + flag_fireplace #+ prop_living + build_land_prop
+                           + factor
+                           , data = house_only16)
+  
+  summary(hedonic_total_fact)
+  
+  hedonic_total_fact <- lm(log(num_tax_total) ~ num_bathroom + num_bedroom + area_live_finished + 
+                             flag_tub_or_spa + area_lot + age + flag_fireplace #+ prop_living + build_land_prop
+                           + factor + num_unit + quality_factor + heating_factor
+                           , data = house_only16_mv)
+  
+  summary(hedonic_total_fact_mv)
 
-# heating id as factor
-p2016 <- left_join(p2016, heating_id, by = 'type_heating')
-p2016 <- p2016[ , -which(names(p2016) %in% c("type_heating"))]
-p2016$heating_factor <- as.factor(p2016$heating_factor)
-
-# type id as factor
-p2016 <- left_join(p2016, quality_id, by = 'type_quality')
-p2016 <- p2016[ , -which(names(p2016) %in% c("type_quality"))]
-p2016$quality_factor <- as.factor(p2016$quality_factor)
-
-# proportion of living area to area lot
-p2016$prop_living <- p2016$area_live_finished/p2016$area_lot
-
-# proportion of building to land value
-p2016$build_land_prop <- p2016$num_tax_building/p2016$num_tax_land
-
-# filter no baths and no bedrooms, we aim to separate properties with buildings
-# and properties without buildings
-property_only16 <- p2016[(p2016$num_bathroom == 0 & p2016$num_bedroom == 0),]
-house_only16 <- p2016[(p2016$num_bathroom > 0 | p2016$num_bedroom > 0),]
-
-## Step 4: Eliminate columns with more than 20% NAs -------------------
-
-# first remove NAs of most important columns to see an more workable data set
-# delete 7126 rows
-house_only16 <- house_only16[(na.omit(house_only16$num_bathroom) &
-                                na.omit(house_only16$num_bedroom) &
-                                na.omit(house_only16$num_tax_total) ), ]
-
-# quick plot
-count_nas <- colSums(is.na(house_only16))/nrow(house_only16)
-sorted <- rev(sort(count_nas))
-barplot(sorted, cex.names = 0.5, las = 2)
-abline(v=35, col="red")
-
-
-# define and select hedonics (<0.2 NAs or <0.4 NAs)
-hedonics <- c('id_parcel','num_bathroom','num_bedroom','area_live_finished',
-              'flag_tub_or_spa','loc_latitude','loc_longitude','area_lot',
-              'factor','loc_zip','loc_county', 'age',
-              'flag_fireplace', 'num_tax_building','num_tax_total',
-              'num_tax_land', 'prop_living', 'build_land_prop')
-
-hedonics2 <- c('id_parcel','num_bathroom','num_bedroom','area_live_finished',
-              'flag_tub_or_spa','loc_latitude','loc_longitude','area_lot',
-              'factor','loc_zip','loc_county', 'age',
-              'flag_fireplace', 'num_tax_building','num_tax_total',
-              'num_tax_land', 'num_unit', 'quality_factor', 'heating_factor',
-              'prop_living', 'build_land_prop')
-
-house_only16_mv <- house_only16 %>% select(hedonics2)
-house_only16 <- house_only16 %>% select(hedonics)
-
-# finally remove all NAs as not usefull for regression and ML
-#house_only16 <- na.omit(house_only16)
-#house_only16_mv <- na.omit(house_only16_mv)
-
-
-## Step 5: Eliminate properties without buildings and very low values ----------
-
-# drop building values below 50'000
-hist(house_only16$num_tax_building[house_only16$num_tax_building < 500000],
-     breaks = 100)
-
-house_only16 <- house_only16[house_only16$num_tax_total >= 50000,]
-
-## Step 5: Plot the variable relationships and remove outliers -------------------------------
-# plot bedroom vs tax
-ggplot(data = house_only16[1:100000,], aes(x = num_bedroom, y = log(num_tax_building))) +
-  geom_point()
-
-# plot bedroom vs tax
-ggplot(data = house_only16[1:100000,], aes(x = num_bathroom, y = log(num_tax_building))) +
-  geom_point()
-
-# plot size vs tax
-ggplot(data = house_only16[1:100000,], aes(x = area_live_finished, y = (num_tax_building))) +
-  geom_point()
-
-# we need to filter the outlier of high area_live finished: 
-house_only16 <- house_only16[house_only16$area_live_finished < 58000,]
-
-ggplot(data = house_only16[1:100000,], aes(x = area_live_finished, y = (num_tax_building))) +
-  geom_point() 
-
-# plot age vs tax
-ggplot(data = house_only16[1:100000,], aes(x = age, y = (log(num_tax_building)))) +
-  geom_point() 
-
-# plot area_lot vs tax
-ggplot(data = house_only16[1:100000,], aes(x = area_lot, y = (num_tax_building))) +
-  geom_point() 
-
-# we need to filter the outlier of high area_lot but very low building structure value 
-house_only16 <- house_only16[house_only16$area_lot < 1e7,]
-
-
-### PART 2 ALGORITHMS ###---------------------------------------------------
-
-## Regressions ------------------------------------
-
-# simple regression of building value
-hedonic_build <- lm(log(num_tax_building) ~ num_bathroom + num_bedroom + area_live_finished + 
-                flag_tub_or_spa + area_lot + age + flag_fireplace #+ prop_living + build_land_prop
-                ,data = house_only16)
-
-summary(hedonic_build)
-
-# simple regression of total value
-hedonic_total <- lm(log(num_tax_total) ~ num_bathroom + num_bedroom + area_live_finished + 
-                flag_tub_or_spa + area_lot + age + flag_fireplace #+ prop_living + build_land_prop
-                ,
-                data = house_only16)
-
-summary(hedonic_total)
-
-
-# lm of building value with factors
-hedonic_total_fact <- lm(log(num_tax_building) ~ num_bathroom + num_bedroom + area_live_finished + 
-                flag_tub_or_spa + area_lot + age + flag_fireplace #+ prop_living + build_land_prop
-                + factor
-                , 
-                data = house_only16)
-
-summary(hedonic_total_fact)
-
-hedonic_total_fact2 <- lm(log(num_tax_building) ~ num_bathroom + num_bedroom + area_live_finished + 
-                           flag_tub_or_spa + area_lot + age + flag_fireplace #+ prop_living + build_land_prop
-                         + factor + num_unit + quality_factor + heating_factor
-                         , 
-                         data = house_only16_mv)
-
-summary(hedonic_total_fact2)
-
-# lm of building value with factors
-hedonic_total_fact <- lm(log(num_tax_total) ~ num_bathroom + num_bedroom + area_live_finished + 
-                           flag_tub_or_spa + area_lot + age + flag_fireplace #+ prop_living + build_land_prop
-                         + factor
-                         , data = house_only16)
-
-summary(hedonic_total_fact)
-
-hedonic_total_fact <- lm(log(num_tax_total) ~ num_bathroom + num_bedroom + area_live_finished + 
-                           flag_tub_or_spa + area_lot + age + flag_fireplace #+ prop_living + build_land_prop
-                         + factor + num_unit + quality_factor + heating_factor
-                         , data = house_only16_mv)
-
-summary(hedonic_total_fact_mv)
+}
 
 ## Advanced Algorithms --------------------------------
 library(xgboost)
