9416970f1ee9637133da081773be8a4a2a460d32
timggraf <tim.g.graf@gmail.com>
2021-04-17 21:50:24 +0200

    xgboost

--- 03_XGBoost.R
+++ 03_XGBoost.R
@@ -27,29 +27,26 @@ tic()
 setwd('/Users/tgraf/Google Drive/Uni SG/Master/Research Seminar /Repository')
 rm(list=ls())
 
-house_only16_mv <- fread('./Data/house_only16_mv.csv', drop = 'V1')
+house_only16_mv <- fread('./Data/2016.csv', drop = 'V1')
 
-colClasses = c(id_parcel = 'numeric',
-               num_bathroom = 'numeric',
+colClasses = c(num_bathroom = 'numeric',
                num_bedroom = 'numeric',
-               area_live_finished = 'numeric',
-               flag_tub_or_spa = 'numeric',
-               loc_latitude = 'numeric',       
+               area_live_finished = 'numeric_log',
+               num_garage = 'numeric', 
+               area_garage = 'numeric',
+               flag_tub_or_spa = 'factor',
+               loc_latitude = 'numeric',
                loc_longitude = 'numeric',
-               area_lot = 'numeric',
-               factor = 'factor',
+               area_lot = 'numeric_log',
+               loc_city = 'numeric',
                loc_zip  = 'numeric',
-               loc_county = 'factor',
-               age = 'numeric',
-               flag_fireplace = 'numeric',
-               num_tax_building = 'numeric',
-               num_tax_total = 'numeric',
-               num_tax_land = 'numeric',
-               num_unit = 'numeric',
-               quality_factor = 'factor',
-               heating_factor = 'factor',
-               prop_living = 'numeric_character',
-               build_land_prop = 'numeric_character')
+               num_pool = 'numeric',
+               num_story = 'numeric',
+               flag_fireplace = 'factor',
+               num_tax_building = 'numeric_log',
+               num_tax_total = 'numeric_log',
+               num_tax_property = 'numeric_log',
+               age = 'numeric_log')
 
 # make conversions
 for (i in colnames(house_only16_mv)) {
@@ -57,12 +54,11 @@ for (i in colnames(house_only16_mv)) {
     house_only16_mv[[i]] <- as.numeric(house_only16_mv[[i]])
   } else if (colClasses[[i]] == 'factor') {
     house_only16_mv[[i]] <- as.factor(house_only16_mv[[i]])
-  } else if (colClasses[[i]] == 'numeric_character'){
-    house_only16_mv[[i]] <- as.numeric(sub(",", ".", house_only16_mv[[i]], fixed = TRUE))
+  } else if (colClasses[[i]] == 'numeric_log'){
+    house_only16_mv[[i]] <- log(as.numeric(house_only16_mv[[i]]))
   }
 }
 
-str(house_only16_mv)
 
 
 ### PART 1: DATA PREPROCESSING ###--------------------------------------
@@ -71,33 +67,39 @@ str(house_only16_mv)
 data = na.omit(house_only16_mv)
 
 # use only first 10'000
-data = data[1:10000,]
+data = (data[1:10000,])
 
-##set the seed to make your partition reproducible
+
+# normalize area_garage
+for (i in 1:nrow(data)){
+  if (data$num_garage[i] > 0) {
+    data$area_garage[i] <- log(data$area_garage[i]) 
+  }
+}
+
+# set the seed to make your partition reproducible
 set.seed(123)
 smp_size <- floor(0.75 * nrow(data)) ## 75% of the sample size
 train_ind <- sample(seq_len(nrow(data)), size = smp_size)
 
 # features we want to omit for the model 
-omit <- c('id_parcel', 'loc_latitude', 'loc_longitude', 'loc_zip', 'loc_county', 'num_tax_building', 'num_tax_land', 'factor', 'build_land_prop')
+omit <- c('loc_latitude', 'loc_longitude', 'loc_zip', 'loc_city', 'num_tax_total', 'num_tax_property')
 
 # Split the data into train and test
 train16 <- data[train_ind,]
 test16 <- data[-train_ind, ]
 
 # define training label = dependent variable
-output_vector = as.matrix(log(train16[,'num_tax_total']))
-test_vector = as.matrix(log(test16[,'num_tax_total']))
+output_vector = as.matrix(log(train16[,'num_tax_building']))
+test_vector = as.matrix(log(test16[,'num_tax_building']))
 
 #omit variables and convert to numeric again
-train16 <- train16 %>% select(-omit)
-#train16 <- data.frame(sapply(train16, as.numeric))
-test16 <- test16 %>% select(-omit)
-#test16 <- data.frame(sapply(test16, as.numeric))
+train16 <- train16 %>% dplyr::select(-omit)
+test16 <- test16 %>% dplyr::select(-omit)
 
 # convert categorical factor into dummy variables using one-hot encoding
-sparse_matrix_train <- sparse.model.matrix(log(num_tax_total)~.-1, data = train16)
-sparse_matrix_test <- sparse.model.matrix(log(num_tax_total)~.-1, data = test16)
+sparse_matrix_train <- sparse.model.matrix(log(num_tax_building)~.-1, data = train16)
+sparse_matrix_test <- sparse.model.matrix(log(num_tax_building)~.-1, data = test16)
 
 # check the dimnames crated by the one-hot encoding
 sparse_matrix_train@Dimnames[[2]]
@@ -106,50 +108,53 @@ sparse_matrix_train@Dimnames[[2]]
 dtrain <- xgb.DMatrix(data = sparse_matrix_train, label = output_vector)
 dtest <- xgb.DMatrix(data = sparse_matrix_test, label=test_vector)
 
+str(train16)
+str(sparse_matrix_train)
+
 
 ### PART 2: XGBOOST Training ###  -----------------------------
 
 # # Model 1: Default parameters  -----------------------------
-#Let's start with a standard model and parameters and start optimizing the parameters later from here
-
-params <- list(booster = "gbtree",
-               objective = "reg:squarederror",
-               eta=0.3, # learning rate, between 0 and 1
-               gamma=0, # regularization (prevents overfitting), higher means more penality for large coef
-               max_depth=6, # max depth of trees, the more deep the more complex and overfitting
-               min_child_weight=1, # min number of instances per child node, blocks potential feature interaction and thus overfitting
-               subsample=1, # number of observations per tree, typically between 0.5 - 0.8
-               colsample_bytree=1) # number of variables per tree, typically between 0.5 - 0.9
-
-# using cross-validation to find optimal nrounds parameter
-xgbcv <- xgb.cv(params = params,
-                data = dtrain,
-                nrounds = 100,
-                nfold = 10,
-                showsd = T, # whether to show standard deviation of cv
-                stratified = F,
-                print_every_n = 1,
-                early_stopping_rounds = 20, # stop if we don't see much improvement
-                maximize = F,
-                verbose = 2)
-
-# Result of best iteration
-xgbcv$best_iteration
-
-# first training with optimized nround
-xgb1 <- xgb.train(params = params,
-                  data = dtrain,
-                  nrounds = xgbcv$best_iteration,
-                  watchlist = list(test = dtest, train = dtrain),
-                  early_stopping_rounds = 20,
-                  maximize = F,
-                  eval_metric = "rmse"
-)
-
-# model prediction
-xgb1_pred <- predict(xgb1, dtest)
-rmse_xgb1 <- sqrt(mean((xgb1_pred - test_vector)^2))
-r2_xgb1 <- 1 - sum((test_vector-xgb1_pred)^2) / sum((test_vector-mean(xgb1_pred))^2)
+# #Let's start with a standard model and parameters and start optimizing the parameters later from here
+# 
+# params <- list(booster = "gbtree",
+#                objective = "reg:squarederror",
+#                eta=0.3, # learning rate, between 0 and 1
+#                gamma=0, # regularization (prevents overfitting), higher means more penality for large coef
+#                max_depth=6, # max depth of trees, the more deep the more complex and overfitting
+#                min_child_weight=1, # min number of instances per child node, blocks potential feature interaction and thus overfitting
+#                subsample=1, # number of observations per tree, typically between 0.5 - 0.8
+#                colsample_bytree=1) # number of variables per tree, typically between 0.5 - 0.9
+# 
+# # using cross-validation to find optimal nrounds parameter
+# xgbcv <- xgb.cv(params = params,
+#                 data = dtrain,
+#                 nrounds = 100,
+#                 nfold = 10,
+#                 showsd = T, # whether to show standard deviation of cv
+#                 stratified = F,
+#                 print_every_n = 1,
+#                 early_stopping_rounds = 20, # stop if we don't see much improvement
+#                 maximize = F,
+#                 verbose = 2)
+# 
+# # Result of best iteration
+# xgbcv$best_iteration
+# 
+# # first training with optimized nround
+# xgb1 <- xgb.train(params = params,
+#                   data = dtrain,
+#                   nrounds = xgbcv$best_iteration,
+#                   watchlist = list(test = dtest, train = dtrain),
+#                   early_stopping_rounds = 20,
+#                   maximize = F,
+#                   eval_metric = "rmse"
+# )
+# 
+# # model prediction
+# xgb1_pred <- predict(xgb1, dtest)
+# rmse_xgb1 <- sqrt(mean((xgb1_pred - test_vector)^2))
+# r2_xgb1 <- 1 - sum((test_vector-xgb1_pred)^2) / sum((test_vector-mean(xgb1_pred))^2)
 
 
 
@@ -163,8 +168,8 @@ for(i in fact_col) set(train16,j=i,value = factor(train16[[i]]))
 for (i in fact_col) set(test16,j=i,value = factor(test16[[i]]))
 
 # create tasks for learner
-traintask <- makeRegrTask(data = train16, target = 'num_tax_total')
-testtask <- makeRegrTask(data = test16, target = 'num_tax_total')
+traintask <- makeRegrTask(data = data.frame(train16), target = 'num_tax_building')
+testtask <- makeRegrTask(data = data.frame(test16), target = 'num_tax_building')
 
 # create dummy features, as classif.xgboost does not support factors
 traintask <- createDummyFeatures(obj = traintask)
@@ -179,6 +184,7 @@ lrn$par.vals <- list(objective="reg:squarederror",
                      eta = 0.1)
 
 # set parameter space
+# for computational reasons we only optimize the most important variables with are the booster type and the max depth per tree
 params <- makeParamSet(makeDiscreteParam("booster", values = c("gbtree", "dart")), 
                        makeIntegerParam("max_depth",lower = 3L,upper = 10L)) 
                        #makeNumericParam("min_child_weight",lower = 1L,upper = 10L), 
@@ -189,12 +195,12 @@ params <- makeParamSet(makeDiscreteParam("booster", values = c("gbtree", "dart")
 
 # set resampling strategy
 # If you have many classes for a classification type predictive modeling problem or the classes are imbalanced 
-#(there are a lot more instances for one class than another), it can be a good idea to create stratified folds when performing cross validation.
+# (there are a lot more instances for one class than another), it can be a good idea to create stratified folds when performing cross validation.
 rdesc <- makeResampleDesc("CV",stratify = F, iters=5L)
 
 # search strategy
-# instead of a grid search we use a random search strategy to find the best parameters
-ctrl <- makeTuneControlRandom(maxit = 10L)
+# instead of a grid search we use a random search strategy to find the best parameters. 
+ctrl <- makeTuneControlRandom(maxit = 10L) #maxit is the number of iterations for random search
 
 # set parallel backend
 parallelStartSocket(cpus = detectCores())
@@ -262,7 +268,7 @@ str(xgb2_pred)
 train16_sparse <- data.frame(model.matrix(~ . -1, train16))
 test16_sparse <- data.frame(model.matrix(~ . -1, test16))
 
-model_lm = lm(num_tax_total ~ ., data = data.frame(train16_sparse))
+model_lm = lm(num_tax_building ~ ., data = data.frame(train16_sparse))
 pred_lm <- log(predict(model_lm, (test16_sparse)))
 
 # Predict using simple regression
@@ -324,23 +330,28 @@ capturing non-linear relationship"
 # 
 
 # COMPARE RMSE and R2 -----------------------------
-comparison <- data.frame(matrix(data = NA, nrow = 3, ncol = 2, dimnames = list(c('xgb_tree 1', 'xgb_tree 2', 'xgb_linear 3'), c('RMSE', 'R2'))))
+comparison <- data.frame(matrix(data = NA, nrow = 4, ncol = 2, dimnames = list(c('xgb_tree 1', 'xgb_tree 2', 'xgb_linear 3', 'linear glm'), c('RMSE', 'R2'))))
 
-comparison$RMSE[1] <- rmse_xgb1
+#comparison$RMSE[1] <- rmse_xgb1
 comparison$RMSE[2] <- rmse_xgb2
-comparison$RMSE[3] <- rmse_xgb3
+#comparison$RMSE[3] <- rmse_xgb3
+comparison$RMSE[4] <- rmse_lm
 
-comparison$R2[1] <- r2_xgb1
+
+#comparison$R2[1] <- r2_xgb1
 comparison$R2[2] <- r2_xgb2
-comparison$R2[3] <- r2_xgb3
+#comparison$R2[3] <- r2_xgb3
+comparison$R2[4] <- r2_lm
+
 
 comparison
 
 # PLOTS --------------------------------------------------
 
-# plot the most important leaflets
-xgb.plot.multi.trees(feature_names = names(dtrain), 
-                     model = xgb1)
+# plot an example tree of all
+xgb.plot.tree(feature_names = names(dtrain), 
+              model = xgb2, 
+              trees = 1)
 
 # Plot importance
 importance2 <- xgb.importance(feature_names = colnames(sparse_matrix_train), model = xgb2)
@@ -351,7 +362,8 @@ plot_xgb_importance <- xgb_importance %>%
                         geom_bar(stat="identity", fill="#f68060", alpha=.6, width=.4) +
                         coord_flip() +
                         xlab("") +
-                        theme_bw()
+                        theme_bw() +
+                        ggtitle('Feature Importance Plot for XG-Boost')
 plot_xgb_importance
 
 # define top 3 relevant variables
@@ -361,40 +373,36 @@ variable3 = xgb_importance$Feature[3]
 
 # merge dataframes
 merged_df <- data.frame(cbind(test_vector, xgb2_pred, test16)) #by 0 merges based on index
-merged_df <- merged_df[order(merged_df$num_tax_total),]
+merged_df <- merged_df[order(merged_df$num_tax_building),]
 merged_df$initialindex <- row.names(merged_df)
 row.names(merged_df) <- NULL
-
-# Plot histograms
-hist(predicted$xgb2_pred)
-hist(actual$num_tax_total)
 
 # Plot predicted vs. actual 
 colors <- c("actual" = "red", "predicted" = "blue")
 plot_xgb <- ggplot(data = merged_df, aes(x = as.numeric(row.names(merged_df)))) +
   geom_point(aes(y = xgb2_pred, color = 'predicted')) +
-  geom_point(aes(y = num_tax_total, color = 'actual')) +
+  geom_point(aes(y = num_tax_building, color = 'actual')) +
   ggtitle('Actual vs. predicted values') + 
   scale_color_manual(values = colors) +
-  labs(x = 'Index', y = 'Log(num_tax_total)')
+  labs(x = 'Index', y = 'Log(num_tax_building)')
 plot_xgb
 
 # Plot most Top 1 variable vs. actual 
 plot_v1 <- ggplot(data = merged_df) +
-  geom_point(aes(x = !!ensym(variable1), y = num_tax_total)) +
-  ggtitle(paste0('Log(num_tax_total) vs. ', variable1))
+  geom_point(aes(x = !!ensym(variable1), y = num_tax_building)) +
+  ggtitle(paste0('Log(num_tax_building) vs. ', variable1))
 plot_v1
 
 # Plot most Top 2 variable vs. actual 
 plot_v2 <- ggplot(data = merged_df) +
-  geom_point(aes(x = !!ensym(variable2), y = num_tax_total)) +
-  ggtitle(paste0('Log(num_tax_total) vs. ', variable2))
+  geom_point(aes(x = !!ensym(variable2), y = num_tax_building)) +
+  ggtitle(paste0('Log(num_tax_building) vs. ', variable2))
 plot_v2
 
 # Plot most Top 3 variable vs. actual 
 plot_v3 <- ggplot(data = merged_df) +
-  geom_point(aes(x = !!ensym(variable3), y = num_tax_total)) +
-  ggtitle(paste0('Log(num_tax_total) vs. ', variable3))
+  geom_point(aes(x = !!ensym(variable3), y = num_tax_building)) +
+  ggtitle(paste0('Log(num_tax_building) vs. ', variable3))
 plot_v3
 
 # # version 2 for plotting
